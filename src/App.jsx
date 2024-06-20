4f259f4a7e9d8ea772b032568c701ce567fa2b22
anas20023 <yorder07@gmail.com>
2024-06-20 19:17:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Heading from "./components/Heading";
+
+const Div = () => {
+  return (<>
+    <Heading pagename={"Calculator"} />
+    <div className="flex flex-col justify-between items-center px-4">
+    </div>
+  </>
+  );
+};
+
+export default Div;
---
+++ src/components/heading.jsx
@@ -0,0 +1,6 @@
+const heading = ({ pagename }) => {
+    return (<>
+        <h1 className="text-center my-4 font-bold text-slate-800 text-4xl">Welcome to {pagename}</h1>
+    </>)
+}
+export default heading;
